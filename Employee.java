6b44fb95607b12077defae2458d1f1c49c5c1110
NehaMNaik <153582377+NehaMNaik@users.noreply.github.com>
2023-12-12 10:48:14 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,17 @@
+class Employee{
+private String firstName;
+private String lastName;
+private String department;
+private String officeLocation;
+private String designation;
+private Double salary;
+
+public Employee(String firstName, String lastName, String department, String officeLocation, String designation, Double salary){
+this.firstName= firstName;
+this.lastName = lastName;
+this.department = department;
+this.officeLocation = ;
+this.designation = designation;
+this.salary = salary;
+}
+}
