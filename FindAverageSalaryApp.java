92dd796248f0b3c624c909e9fdc8af99302c4065
NehaMNaik <153582377+NehaMNaik@users.noreply.github.com>
2023-12-12 10:51:48 +0530

    Update and rename EmpLocAvgSal.java to FindAverageSalaryApp.java

---
+++ FindAverageSalaryApp.java
@@ -0,0 +1,16 @@
+public class FindAverageSalaryApp{
+
+public void findAverageSalary(List<Employee> employees){
+
+HashMap<String, String> empRoleSalMap = new HashMap<>();
+
+for (Employee e : employees) {
+  if (e.getF
+
+}
+
+  
+
+}
+
+}
